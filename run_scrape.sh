107839cb6733d9170d0195bafe2084227305cb69
Ignis-Altum <69248235+Ignis-Altum@users.noreply.github.com>
2022-09-10 20:33:41 +0200

    Update from Raspi

--- run_scrape.sh
+++ run_scrape.sh
@@ -4,6 +4,7 @@ cd /home/pi/Documents/scraper_news/
 git pull upstream main --commit --no-edit
 
 python3 main.py -s --source tv2 --breaking
+python3 main.py -s --source dr
 
 git add scraper_news/news.json
 
